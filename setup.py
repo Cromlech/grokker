78e6f4d2801b884d699cf83a05f9fe05f991211c
Souheil CHELFOUH <trollfot@gmail.com>
2016-12-01 17:24:53 +0100

    Updated version to match normalized format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='grokker',
-      version = '0.1dev',
+      version = '0.1.dev0',
       description="A reformulation of Martian based on Venusian.",
       author="Martijn Faassen",
       author_email="faassen@startifact.com",
